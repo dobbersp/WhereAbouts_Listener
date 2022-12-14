a7622e6ce4e545b5e53c2f27c0ce095aa946031b
dobbersp <dobbersp@gmail.com>
2022-12-14 01:32:27 -0800

    Add files via upload

---
+++ listener.js
@@ -0,0 +1,40 @@
+const https = require("https");
+const io = require("socket.io");
+const fs = require("fs");
+const config = require("config");
+
+var certDir = config.get("certDir");
+var dataDir = config.get("dataDir");
+var serverPort = config.get("serverPort");
+var token = config.get("token");
+
+var options = {
+  key: fs.readFileSync(certDir+"privkey.pem"),
+  cert: fs.readFileSync(certDir+"fullchain.pem")
+};
+
+var https_server = https.createServer(options);
+socket_listener = io.listen(https_server);
+https_server.listen(serverPort);
+
+socket_listener.sockets.on("connection", function(client) {
+  console.log("user connected: " + client.request.connection.remoteAddress);
+  client.on("beacon_speaker", (data) =>handleBeaconData(data))
+});
+
+function handleBeaconData(data)
+{
+  try
+  {
+    if (data.token == token)
+      var dataFileName = data.file;
+      var fileData = data.data;
+	  console.log("Updating " + data.file + " : " + data.data);
+      fs.writeFileSync(dataDir+"/"+dataFileName,JSON.stringify(fileData));
+  }
+  catch(e)
+  {
+    console.log(e.toString());
+    console.log("Malformed beacon message.");
+  }
+}
